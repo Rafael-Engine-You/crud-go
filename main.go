f0c705fad6c3186e3d46599a2d814959aadd4a82
Glêsio Santos <glesioss@gmail.com>
2026-08-24 10:49:56 -0300

    criando modulo de produtos e cadastrando uma nova categoria

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	// "log"
 	"github.com/jackc/pgx/v5/pgxpool"
-	cli "crud-go/clientes"
+	p "crud-go/produtos"
 )
 
 func main() {
@@ -19,27 +19,14 @@ func main() {
 
 	defer db.Close()
 
-	// cliente := cli.Cliente{
-	// 	Nome: "José Pessoa Leal - Dr. Pessoa",
-	// 	Email: "drpessoa@gmail.com",
-	// 	Telefone: "86988556622",
-	// }
-
-	// err = cli.CadastrarCliente(db, cliente)
-
-	cliente, err := cli.CarregarClientePeloId(db, 1)
-	if err != nil {
-		log.Fatal("Erro ao conectar ", err )
+	categoria := p.Categoria {
+		Nome: "Tecnológia",
 	}
 
-	fmt.Printf("%d - %s - %s - %s\n", cliente.Id, cliente.Nome, cliente.Email, cliente.Telefone)
-
-	// for _, cliente := range clientes  {
-	// 	fmt.Println(
-	// 		cliente.Id,
-	// 		cliente.Nome,
-	// 		cliente.Email,
-	// 		cliente.Telefone,
-	// 	)
-	// }
+	err = p.AddCategoria(db, categoria)
+	
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Produto cadastrado com sucesso!")
 }	
